73eaa73ca19abe9ece4d975bbd23a71039e4c670
Jubair Bokht <108237500+mdjubairbokht@users.noreply.github.com>
2026-07-04 00:40:46 -0400

    Add loop examples in index.js
    Added examples of for loop, while loop, and for-of loop.

--- 6Loops/index.js
+++ 6Loops/index.js
@@ -3,12 +3,14 @@ for (let i = 0; i < 5; i++) {
   console.log(i); // prints 0 to 4
 }
 
+
 console.log('while true loop')
 let i = 0;
 while (i < 5) {
   console.log(i);
   i++;
 }
+
 
 console.log('iterate over an array - for of loop')
 const arr = [10, 20, 30];
