0f3fc0c5174a94795edf84b9f10dda9b958939ba
Jubair Bokht <108237500+mdjubairbokht@users.noreply.github.com>
2026-08-17 21:55:42 -0400

    Remove console.log statements from HTTP client

--- 9Http/node/client.js
+++ 9Http/node/client.js
@@ -4,8 +4,9 @@ const url = 'http://127.0.0.1:3000/';
 async function makeGetRequest() {
         const res = await fetch(url);
         const text = await res.text();
-        console.log(text)
+        
 }
+
 
 async function makePostRequest() {
     const response = await fetch(url, {
@@ -15,6 +16,7 @@ async function makePostRequest() {
 
             
     })
+        
     const responseBody = await response.json();
     console.log('POST status:', response.status);
     console.log('POST body:', responseBody);    
